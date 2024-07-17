06105d60dca9949822838cd445a44879ef42c46c
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-17 18:23:49 +0530

    appwrite services

--- src/appwrite/auth_service.js
+++ src/appwrite/auth_service.js
@@ -10,7 +10,54 @@ export class AuthService{
                 .setProject(conf.appwriteProjectId)       
                 this.account=new Account(this.client)           // add client value to account variable
     }
+
+//   create user account method
+  async createAccount({email, password,name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(),email,password,name );
+            if(userAccount){
+                // call another method
+                return this.login(email, password)
+            }
+            else{
+                return userAccount
+            }
+        }
+        catch(error){
+            throw error
+        }
 }
+//  User login method
+ async login(email, password){
+    try{
+        return await this.account.createEmailSession(email, password)
+    }catch(error){
+        throw error
+    }
+ } 
+  
+//  current user available is and  not  in Home Page
+ async getCurrentUser(){
+    try{
+      return await this.account.get();
+    }catch(error){
+        throw error
+    }
+    return null;
+  }
+
+// user LogOut Methed 
+async logout(){
+    try{
+     await this.account.deleteSessions();
+    }catch(error){
+        console.log("logOut appwrite error ::",error)
+    }
+}
+
+}
+
+
 
 const authservice= new AuthService()
 
