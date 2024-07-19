e85c5df317c924d21fec42e24ec41fc99e61afc2
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-19 16:08:29 +0530

    create bucket services

--- src/appwrite/database_service.js
+++ src/appwrite/database_service.js
@@ -94,6 +94,41 @@ export class Service{
         }
 
     }
+    //  Another  Upload file service
+    async uploadFile(file){
+        try {
+            return await  this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite  Database service:: UPLOAD FILE",error);
+                return false
+        }
+    }
+
+//  DELETE FILE Service
+   async deleteFile(fileId){
+    try {
+        await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileId
+        )
+        return true
+    } catch (error) {
+        console.log("Appwrite  Database service:: DELETE FILE",error);
+        return false
+    }
+   }
+
+//    File Previews
+   getFilePreview(fileId){
+    return this.bucket.getFilePreview(
+        conf.appwriteBucketId,
+        fileId
+    )
+   }
 }
 
 const  service = new Service();
